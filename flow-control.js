b94889c668b92d7905a65235c7d174433a5283ad
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-06-19 03:18:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,12 +12,12 @@ return ('You are not a teenager')
   }
 }
 
-function ageChecker(age) {
-if (age >= 13 && age <= 19){
+function ageChecker(age){
+if (age >= 13 && age <= 19){ // age is greater than or equal to 13 or less than or equal to 19
 return ('You are a teenager!')
 } else if (age < 12){
   return ('You are a kid')
-} else {
+} else if (age > 20) {
 return ('You are a grownup')
   }
 }
