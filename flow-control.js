544978747dd738d8bfd2ffc4ffc5d36d8aca3d7c
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-06-19 03:54:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -35,6 +35,6 @@ Switch (age)
 case "You are a teenager"  // You are a teenager if age is greater than or equal to 13 || age is less than or equal to 19
   console.log(age >= 13 && age <= 19);
     break;
-    default: 
-      console.log('You have an age'); 
+    default:
+      console.log('You have an age');
 }
